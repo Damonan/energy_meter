d1734069ff2f599cea622fd54408d8d4d8eba40b
Damon Anderson <damonan@berkeley.edu>
2018-10-24 15:01:51 -0700

    changed to revA

--- software/apps/light_sensor/_build/ff.d
+++
@@ -1,7 +0,0 @@
-_build/ff.o: \
- ../../buckler/software/nrf5x-base//lib/simple_logger/chanfs/ff.c \
- ../../buckler/software/nrf5x-base//lib/simple_logger/chanfs/ff.h \
- ../../buckler/software/nrf5x-base//lib/simple_logger/chanfs/integer.h \
- ../../buckler/software/nrf5x-base//lib/simple_logger/chanfs/ffconf.h \
- ../../buckler/software/nrf5x-base//lib/simple_logger/chanfs/diskio.h \
- /home/tim/opt/gcc-arm-none-eabi-6-2017-q2-update/lib/gcc/arm-none-eabi/6.3.1/include/stdarg.h
