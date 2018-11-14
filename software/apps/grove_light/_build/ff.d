64bb2110b9ec39e9becfd2d6f40f66f0f2d5c1df
timothybarat <timothybarat@hotmail.com>
2018-11-13 21:42:30 -0800

    moved the location of buckler repo and added ble_tx_example

--- software/apps/grove_light/_build/ff.d
+++ software/apps/grove_light/_build/ff.d
@@ -1,7 +1,7 @@
 _build/ff.o: \
- ../../../buckler/software/nrf5x-base//lib/simple_logger/chanfs/ff.c \
- ../../../buckler/software/nrf5x-base//lib/simple_logger/chanfs/ff.h \
- ../../../buckler/software/nrf5x-base//lib/simple_logger/chanfs/integer.h \
- ../../../buckler/software/nrf5x-base//lib/simple_logger/chanfs/ffconf.h \
- ../../../buckler/software/nrf5x-base//lib/simple_logger/chanfs/diskio.h \
- /usr/lib/gcc/arm-none-eabi/7.3.1/include/stdarg.h
+ ../../buckler/software/nrf5x-base//lib/simple_logger/chanfs/ff.c \
+ ../../buckler/software/nrf5x-base//lib/simple_logger/chanfs/ff.h \
+ ../../buckler/software/nrf5x-base//lib/simple_logger/chanfs/integer.h \
+ ../../buckler/software/nrf5x-base//lib/simple_logger/chanfs/ffconf.h \
+ ../../buckler/software/nrf5x-base//lib/simple_logger/chanfs/diskio.h \
+ /home/tim/opt/gcc-arm-none-eabi-6-2017-q2-update/lib/gcc/arm-none-eabi/6.3.1/include/stdarg.h
