ad9cd50840302bd8129e6dd0bf5b84d79d1d1eca
timothybarat <timothybarat@hotmail.com>
2018-11-14 17:42:40 -0800

    added gateway_ble and update ble_tx

--- software/apps/ble_tx_example/main.c
+++ software/apps/ble_tx_example/main.c
@@ -13,7 +13,7 @@ APP_TIMER_DEF(adv_timer);
 
 // BLE configuration
 static simple_ble_config_t ble_config = {
-        // BLE address is c0:98:e5:49:00:01
+        // BLE address is c0:98:e5:49:00:03
         .platform_id       = 0x49,    // used as 4th octet in device BLE address
         .device_id         = 0x0003,  // used as the 5th and 6th octet in the device BLE address, you will need to change this for each device you have
         .adv_name          = "EE149", // irrelevant in this example
@@ -22,6 +22,10 @@ static simple_ble_config_t ble_config = {
         .max_conn_interval = MSEC_TO_UNITS(1000, UNIT_1_25_MS), // irrelevant if advertising only
 };
 simple_ble_app_t* simple_ble_app;
+
+static uint8_t count_buff[4];
+static uint8_t count_buff_len = 4;
+static uint32_t count;
 
 
 // Sends the specified data over BLE advertisements
@@ -52,24 +56,27 @@ void set_ble_payload(uint8_t* buffer, uint8_t length) {
   simple_ble_set_adv(&advdata, NULL);
 }
 
-// Callback when the timer fires. Updates the advertisement data
-void adv_timer_callback(void) {
-  static uint8_t increment = 0;
-
-  // Update advertisement data
-  // Increments each value by one each time
-  increment++;
-  uint8_t buffer[23] = {0};
-  for (int i=0; i<23; i++) {
-    buffer[i] = i+increment;
-  }
-  printf("Buffer value: %d\n", buffer[0]);
-  set_ble_payload(buffer, 23);
+void update_count_buffer(void) {
+  count_buff[0] = (count >> 24) & 0xFF;
+  count_buff[1] = (count >> 16) & 0xFF;
+  count_buff[2] = (count >> 8) & 0xFF;
+  count_buff[3] = count & 0xFF;
 }
 
+// Callback when the timer fires. Updates the advertisement data
+void adv_timer_callback(void) {
+  // Update advertisement data
+  // Increments each value by one each time
+  count += 2;
+  update_count_buffer();
+  set_ble_payload(count_buff, count_buff_len);
+}
+
+
 void update_advertisement(void) {
-	uint8_t buffer[23] = 'Infrared Pulse Counter!';
-	set_ble_payload(buffer, 23);
+	count += 1;
+  update_count_buffer();
+	set_ble_payload(count_buff, count_buff_len);
 }
 
 
@@ -79,20 +86,13 @@ int main(void) {
   // Note: simple BLE is our own library. You can find it in `nrf5x-base/lib/simple_ble/`
   simple_ble_app = simple_ble_init(&ble_config);
 
-  // Set advertisement data. Maximum size is 23 bytes of payload
-  // uint8_t buffer[23] = {0};
-  // for (int i=0; i<23; i++) {
-  //   buffer[i] = i;
-  // }
-  // set_ble_payload(buffer, 23);
-
   update_advertisement();
 
   // Set a timer to change the data. Data could also be changed after sensors
   // are read in real applications
-  // app_timer_init();
-  // app_timer_create(&adv_timer, APP_TIMER_MODE_REPEATED, (app_timer_timeout_handler_t)adv_timer_callback);
-  // app_timer_start(adv_timer, APP_TIMER_TICKS(1000), NULL); // 1000 milliseconds
+  app_timer_init();
+  app_timer_create(&adv_timer, APP_TIMER_MODE_REPEATED, (app_timer_timeout_handler_t)adv_timer_callback);
+  app_timer_start(adv_timer, APP_TIMER_TICKS(1000), NULL); // 1000 milliseconds
 
   // go into low power mode
   while(1) {
